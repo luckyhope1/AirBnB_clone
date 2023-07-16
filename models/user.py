381803c83f58bb70680f603cba76702eb71ee800
Prof. Percival <110649262+Prof-Percival@users.noreply.github.com>
2023-07-16 17:19:57 +0200

    user

---
+++ models/user.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""User Class"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """A User class"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
